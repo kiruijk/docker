092a625a1e93f7556d8d9e302b3dc537e33b7e25
James Kirui <jameskirui@gmail.com>
2022-03-20 00:35:28 -0400

    first commit

---
+++ docker.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Desctription: Automates installation of Docker in Centos Linux.
+#Author: James K
+#Date: March 20, 2022
+
+#Clean the system i.e. removing old versions of docker that may be installed
+echo "Cleaning the system...."
+sleep 2
+sudo yum remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-engine
+
+#Set up docker repository
+echo "Setting up docker repository...."
+slesudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repoep 2
+
+#Install docker engine
+echo "Installing the latest version of docker engine...."
+sleep 2
+sudo yum install docker-ce docker-ce-cli containerd.io
+
+if [ $? -ne 0 ]
+then
+echo "Docker installation failed..."
+sleep 2
+exit 2
+fi
+
+#Check status, start and enable docker deamon
+echo "Checking status, stating and enabling docker deamon...."
+sleep 2
+sudo systemctl status docker
+sleep 2
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sleep 2
+
+echo "Docker installation in your system is complete!"
